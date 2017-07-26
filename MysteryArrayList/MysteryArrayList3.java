e09dccad77c9a892eb2da5fcf63b4b4f609963a8
Florian Biermann <fbie@itu.dk>
2017-07-26 16:28:12 +0200

    Add MysteryArrayList3

---
+++ MysteryArrayList/MysteryArrayList3.java
@@ -0,0 +1,42 @@
+package dk.itu.pcpp;
+
+public class MysteryArrayList3<T> implements IMysteryArrayList<T> {
+
+    private final T[] array;
+    private int last;
+
+    /**
+     * Create a new MysteryArrayList3 instance.
+     * @param array The array to back this MysteryArrayList3 instance.
+     */
+    public MysteryArrayList3(T[] array) {
+        this.array = array;
+        this.last = 0;
+    }
+
+    @Override
+    public synchronized T get(int i) {
+        return array[i];
+    }
+
+    @Override
+    public synchronized boolean append(T value) {
+        if (last < array.length) {
+            array[last++] = value;
+            return true;
+        } else
+            return false;
+    }
+
+    @Override
+    public synchronized int length() {
+        return last;
+    }
+
+    @Override
+    public synchronized void clear() {
+        for (int i = 0; i < last; ++i)
+            array[i] = null;
+        last = 0;
+    }
+}
