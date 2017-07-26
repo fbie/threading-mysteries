e1b11c5536197c462c01bb258b962113ccb9d3af
Florian Biermann <fbie@itu.dk>
2017-07-26 15:36:44 +0200

    Build a more sustainable build.sh

--- MysteryCounter/build.sh
+++ MysteryCounter/build.sh
@@ -1,3 +1,18 @@
-#+/bin/bash
+#!/bin/bash
 
-javac IMysteryCounter.java MysteryCounter{1,3}.java
+# Everything is named as the containing folder.
+NAME=$(basename $(pwd))
+
+# Start fresh.
+rm -rf bin javadoc $NAME.jar $NAME.zip
+mkdir -p bin javadoc
+
+# Compile and build docs.
+javac -d bin *.java
+javadoc -d javadoc *.java
+
+# Build a JAR file.
+jar cf $NAME.jar bin/dk/itu/pcpp/*.class
+
+# Build a distributable archive.
+zip -r $NAME.zip javadoc $NAME.jar
