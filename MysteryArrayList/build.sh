423ae2f3f3be4b2c8a650f8e27a26c4c786bb84c
Florian Biermann <fbie@itu.dk>
2017-07-27 12:23:56 +0200

    Update and correct build scripts

--- MysteryArrayList/build.sh
+++ MysteryArrayList/build.sh
@@ -2,14 +2,14 @@
 
 # Everything is named as the containing folder.
 NAME=$(basename $(pwd))
-mkdir -p bin javadoc
+mkdir -p bin bin/javadoc
 
 # Compile and build docs.
 javac -d bin *.java
-javadoc -d javadoc *.java
+javadoc -d bin/javadoc *.java
 
 # Build a JAR file.
-jar cf $NAME.jar bin/dk/itu/pcpp/*.class
+(cd bin; jar cfv $NAME.jar dk/itu/pcpp/*.class)
 
 # Build a distributable archive.
-zip -r $NAME.zip javadoc $NAME.jar
+zip -r $NAME.zip bin/javadoc bin/$NAME.jar
