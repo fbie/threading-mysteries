5845d8b160fe849ccc8cf4db9e6967a8500aa217
Florian Biermann <flbm@simcorp.com>
2025-01-28 14:31:43 +0100

    Format sources with LSP

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,8 +1,10 @@
-﻿if (args.Length != 3) {
-    Console.WriteLine("Usage: 03 [A|B|C] <number-of-threads> <number-of-increments>");
+﻿if (args.Length != 3)
+{
+    Console.WriteLine("Usage: 04 [A|B|C] <number-of-threads> <number-of-increments>");
     return;
 }
-ICounter counter = args[0] switch {
+ICounter counter = args[0] switch
+{
     "A" => new InterlockedCounter(),
     "B" => new PaddedCounter(),
     "C" => new LockingCounter(),
@@ -11,10 +13,13 @@ ICounter counter = args[0] switch {
 var threads = int.Parse(args[1]);
 var increments = int.Parse(args[2]);
 var barrier = new Barrier(threads + 1);
-for (var i = 0; i < threads; i++) {
-    Task.Run(() => {
+for (var i = 0; i < threads; i++)
+{
+    Task.Run(() =>
+    {
         barrier.SignalAndWait();
-        for (var j = 0; j < increments; j++) {
+        for (var j = 0; j < increments; j++)
+        {
             counter.Increment();
         }
         barrier.SignalAndWait();
@@ -27,37 +32,44 @@ barrier.SignalAndWait();
 sw.Stop();
 Console.WriteLine($"Counter value: {counter.Value}\nTook {sw.ElapsedMilliseconds}ms");
 
-interface ICounter {
+interface ICounter
+{
     long Value { get; }
     void Add(long value);
     void Increment();
 }
 
-class LockingCounter : ICounter {
+class LockingCounter : ICounter
+{
     private readonly object _lock = new object();
     private long _value = 0L;
-    public long Value { get { lock (_lock) { return _value; }}}
-    public void Add(long value) { lock (_lock) { _value += value; }}
+    public long Value { get { lock (_lock) { return _value; } } }
+    public void Add(long value) { lock (_lock) { _value += value; } }
     public void Increment() => Add(1L);
 }
 
-class InterlockedCounter : ICounter {
+class InterlockedCounter : ICounter
+{
     private long _value = 0L;
     public long Value => Interlocked.Read(ref _value);
     public void Add(long value) => Interlocked.Add(ref _value, value);
     public void Increment() => Add(1L);
 }
 
-class PaddedCounter : ICounter {
+class PaddedCounter : ICounter
+{
     private const int CACHE_LINE_SIZE = 64 * 8;
     private const int STRIPES = 31;
     private const int PADDING = CACHE_LINE_SIZE / sizeof(long);
     private readonly long[] _values = new long[STRIPES * PADDING];
 
-    public long Value {
-        get {
+    public long Value
+    {
+        get
+        {
             var sum = 0L;
-            for (var i = 0; i < _values.Length; i+= PADDING) {
+            for (var i = 0; i < _values.Length; i += PADDING)
+            {
                 sum += Interlocked.Read(ref _values[i]);
             }
             return sum;
