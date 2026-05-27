6aee7520b3ccb0d4f2e776916a2b017f12e60ccb
Florian Biermann <florian.biermann@protonmail.com>
2026-05-27 22:03:20 +0200

    Use a padded struct instead of empty array slots

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,4 +1,6 @@
-﻿#pragma warning disable CS4014
+﻿using System.Runtime.InteropServices;
+
+#pragma warning disable CS4014
 if (args.Length != 3)
 {
     Console.WriteLine("Usage: 04.exe [A|B|C] <number-of-threads> <number-of-increments>");
@@ -59,26 +61,37 @@ class InterlockedCounter : ICounter
 
 class PaddedCounter : ICounter
 {
-    private const int CACHE_LINE_SIZE = 64 * 8;
-    private const int STRIPES = 31;
-    private const int PADDING = CACHE_LINE_SIZE / sizeof(long);
-    private readonly long[] _values = new long[STRIPES * PADDING];
+    [StructLayout(LayoutKind.Sequential, Size=64)]
+    struct PaddedLong {
+        public long Value; // 8 bytes payload +
+        private byte        // 56 bytes padding = 64 bytes.
+            _p01, _p02, _p03, _p04, _p05, _p06, _p07, _p08,
+            _p09, _p10, _p11, _p12, _p13, _p14, _p15, _p16,
+            _p17, _p18, _p19, _p20, _p21, _p22, _p23, _p24,
+            _p25, _p26, _p27, _p28, _p29, _p30, _p31, _p32,
+            _p33, _p34, _p35, _p36, _p37, _p38, _p39, _p40,
+            _p41, _p42, _p43, _p44, _p45, _p46, _p47, _p48,
+            _p49, _p50, _p51, _p52, _p53, _p54, _p55, _p56;
+    }
+
+    private const int STRIPES = 32;
+    private readonly PaddedLong[] _values = new PaddedLong[STRIPES];
 
     public long Value
     {
         get
         {
             var sum = 0L;
-            for (var i = 0; i < _values.Length; i += PADDING)
+            for (var i = 0; i < _values.Length; i++)
             {
-                sum += Interlocked.Read(ref _values[i]);
+                sum += Interlocked.Read(ref _values[i].Value);
             }
             return sum;
         }
     }
 
     public void Add(long value)
-        => Interlocked.Add(ref _values[Thread.CurrentThread.ManagedThreadId % STRIPES * PADDING], value);
+        => Interlocked.Add(ref _values[Thread.CurrentThread.ManagedThreadId % STRIPES].Value, value);
 
     public void Increment() => Add(1L);
 }
