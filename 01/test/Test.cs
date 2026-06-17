b572b287c57efc913bd29c4fb21f37f94a573b78
Florian Biermann <florian.biermann@protonmail.com>
2026-06-17 09:33:30 +0200

    Refactory mystery 01 tests

--- 01/test/Test.cs
+++ 01/test/Test.cs
@@ -3,19 +3,28 @@
 [TestClass]
 public sealed class Test
 {
-    [DataRow(10)]
-    [TestMethod]
-    public void TestSequence(int n)
+
+    public void TestSequenceBuilder(Func<int, ISequenceBuilder> f)
     {
+        var n = 10;
         var seq = new int[n];
         for (var i = 0; i < n; i++) {
             seq[i] = i + 1;
         }
 
-        var b = new SequenceBuilderA(n); // SequenceBuilderB, SequenceBuilderC ...
+        var b = f(n);
         var t = new Thread(() => { while (b.Next()) { } });
         t.Start();
         t.Join();
         CollectionAssert.AreEqual(seq, b.Sequence);
     }
+
+    [TestMethod]
+    public void TestSequenceBuilderA() => TestSequenceBuilder(n => new SequenceBuilderA(n));
+
+    [TestMethod]
+    public void TestSequenceBuilderB() => TestSequenceBuilder(n => new SequenceBuilderB(n));
+
+    [TestMethod]
+    public void TestSequenceBuilderC() => TestSequenceBuilder(n => new SequenceBuilderC(n));
 }
