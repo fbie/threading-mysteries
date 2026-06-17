383a3788c28df68d45fa5f24d9963e5c5aa2f81f
Florian Biermann <florian.biermann@protonmail.com>
2026-06-17 09:13:41 +0200

    Fix run script

--- 06/test/Test.cs
+++ 06/test/Test.cs
@@ -4,15 +4,23 @@ using System.Threading;
 [TestClass]
 public sealed class Test
 {
-    [DataRow(1)]
+
+    [DynamicData(nameof(Waiters))]
+    [Timeout(5000)]
     [TestMethod]
-    public void TestWaiter(int n)
+    public void TestWaiter(IWaiter waiter)
     {
+        var n = 10;
         var barrier = new Barrier(n + 1);
-        var waiter = IWaiter.MakeA(); // MakeB...
         for (var i = 0; i < n; i++) {
             waiter.Wait(barrier);
         }
         barrier.SignalAndWait();
     }
+
+    public static IEnumerable<IWaiter> Waiters() {
+        yield return IWaiter.MakeA();
+        yield return IWaiter.MakeB();
+    }
+
 }
